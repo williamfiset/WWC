26f542eae85e2f83049c2eb33bebb2deee1de1f8
aknecht2 <aknecht2@unl.edu>
2017-10-21 11:22:32 -0700

    asdf

--- server.py
+++ server.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/json/<string:file_name>/")
+def render(file_name):
+    if not file_name.endswith(".json"):
+        file_name = "%s.json" % (file_name,)
+    return render_template(file_name)
+
+
 if __name__ == "__main__":
     app.run()
