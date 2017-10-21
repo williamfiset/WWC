a703ccf3cd9ba34855f99815a97de90c01723642
aknecht2 <aknecht2@unl.edu>
2017-10-21 15:39:03 -0700

    Tooolllttipiipipiip

--- static/js/usachart.js
+++ static/js/usachart.js
@@ -16,7 +16,7 @@ var map = AmCharts.makeChart( "chartdiv", {
     "autoZoom": false,
     "selectedColor": "#CC0000",
     "selectable": true,
-    "balloonText": "Average Annual Income: $[[value]]",
+    "balloonText": "<b>[[title]]</b><br />Average Annual Income: $[[value]]",
     "descriptionWindowWidth": 0,
     "descriptionWindowHeight": 0
   },
