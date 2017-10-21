28f634a5be3805f2a6c399e0346d8c52ff781480
aknecht2 <aknecht2@unl.edu>
2017-10-21 12:33:51 -0700

    Added end point for heat map data.

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, jsonify
+import os
 app = Flask(__name__)
+
+SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
 
 @app.route("/")
 def index():
@@ -9,8 +12,16 @@ def index():
 def render(file_name):
     if not file_name.endswith(".json"):
         file_name = "%s.json" % (file_name,)
-    return render_template(file_name)
+    return render_template("json/%s" % (file_name,))
 
+@app.route("/heat_map")
+def muh_data():
+    with open(os.path.join(SITE_ROOT, "templates/json/state_M2016_total.json"), "r") as rh:
+        data = json.load(rh)
+    print(data)
+    # DO STUFF HERE
+
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run()
